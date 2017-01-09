628e460566d32c4ab759d6e80757b526ecc670cd
Oshane Thomas <othomas2@illinois.edu>
2017-01-09 16:21:16 -0600

    Update imus.py
    hopefully, the last trapz fix

--- skinematics/imus.py
+++ skinematics/imus.py
@@ -222,8 +222,8 @@ class IMU:
         pos = np.nan*np.ones_like(accReSpace)
      
         for ii in range(accReSpace.shape[1]):
-            vel[:,ii] = trapz(y = accReSpace[:,ii], dx=1./np.float(self.rate), initial=0) #x = self.deltaT[:,ii]
-            pos[:,ii] = trapz(y = vel[:,ii],        dx=1./np.float(self.rate), initial=initialPosition[ii]) #x = self.deltaT[:,ii]
+            vel[:,ii] = trapz(y = accReSpace[:,ii], x = self.deltaT[:,ii]) #x = self.deltaT[:,ii] #, initial=0
+            pos[:,ii] = trapz(y = vel[:,ii], x = self.deltaT[:,ii]) # dx=1./np.float(self.rate)
  
         self.vel = vel # save velocity OT
         self.pos = pos
